01bb110da166e8d1d26f5b90b9c046c62304c5d8
Miguel <miguelgasang@gmail.com>
2024-02-04 06:56:52 +0800

    123

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,8 +24,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h2 className="h2-bold">Post Your Events, <br/>Spark Connections!</h2>
+            <p className="p-regular-15 md:p-regular-15">Whatever your interest, from hiking and reading to networking and skill sharing, there are thousands of people who share it on Eventus. Events are happening every day—sign up to join the fun.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -44,8 +44,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
-
+        <h2 className="h2-bold"> <br />Discover Events</h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <CategoryFilter />
